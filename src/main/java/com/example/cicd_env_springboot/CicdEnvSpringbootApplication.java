dfd59ed9c511d2e3713ccf4cd93cf8ba80356033
Dennis de Oliveira <contato@dennisoliveira.com.br>
2025-08-02 17:31:32 -0300

    first commit

---
+++ src/main/java/com/example/cicd_env_springboot/CicdEnvSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_env_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdEnvSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdEnvSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicd_env_springboot/CicdEnvSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_env_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdEnvSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
